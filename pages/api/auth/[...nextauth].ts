2ecf65fc8f769be1e834f401f11327ebfff222e7
ypapy <ypapy22@gmail.com>
2023-05-06 09:37:55 +0200

    Resolving a deployemnt issue

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -41,7 +41,7 @@ export default NextAuth({
 
                 if(!user)
                     throw new Error("Email does not exist")
-                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword || '');
                 
                 if(!isCorrectPassword)
                     throw new Error("Incorrect password");
